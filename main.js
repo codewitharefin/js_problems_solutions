0e7e99fd1a585c8080171a5267967008c95ea86b
Md. Morshedul Arefin <arefin2k@gmail.com>
2023-07-19 02:20:17 +0600

    initial

---
+++ main.js
@@ -0,0 +1,59 @@
+// ✅ Problem 1: Write a function to find the maximum number in an array of numbers.
+
+// ✅ Problem 2: Write a function to reverse a string.
+
+// ✅ Problem 3: Write a function to check if a number is prime.
+
+// ✅ Problem 4: Write a function to calculate the sum of all elements in an array.
+
+// ✅ Problem 5: Write a function to find the average of all elements in an array.
+
+// ✅ Problem 6: Write a function to check if a number is even.
+
+// ✅ Problem 7: Write a function to find the factorial of a number.
+
+// ✅ Problem 8: Write a function to check if a string is a palindrome.
+
+// ✅ Problem 9: Write a function to remove duplicate elements from an array.
+
+// ✅ Problem 10: Write a function to find the second smallest element in an array.
+
+// ✅ Problem 11: Write a function to find the common elements between two arrays.
+
+// ✅ Problem 12: Write a function to convert a string to title case.
+
+// ✅ Problem 13: Write a function to find the longest word in a string.
+
+// ✅ Problem 14: Write a function to remove a specific element from an array.
+
+// ✅ Problem 15: Write a function to find the Fibonacci sequence up to a given number of terms.
+
+// ✅ Problem 16: Write a function to check if a string is a pangram.
+
+// ✅ Problem 17: Write a function to capitalize the first letter of each word in a string.
+
+// ✅ Problem 18: Write a function to count the number of vowels in a string.
+
+// ✅ Problem 19: Write a function to find the number of words in a string.
+
+// ✅ Problem 20: Write a function to check if a string is a valid URL.
+
+// ✅ Problem 21: Write a function to find the sum of digits of a number.
+
+// ✅ Problem 22: Write a function to find the number of occurrences of a character in a string.
+
+// ✅ Problem 23: Write a function to generate a random integer between two given numbers (inclusive).
+
+// ✅ Problem 24: Write a function to find the sum of all even numbers in an array.
+
+// ✅ Problem 25: Write a function to find the difference between the largest and smallest elements in an array.
+
+// ✅ Problem 26: Write a function to check if a number is a power of two.
+
+// ✅ Problem 27: Write a function to reverse the order of words in a string.
+
+// ✅ Problem 28: Write a function to check if a number is a Armstrong number. An Armstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+
+// ✅ Problem 29: Write a function to merge two sorted arrays into a new sorted array.
+
+// ✅ Problem 30: Write a function to find the sum of all prime numbers in an array.
